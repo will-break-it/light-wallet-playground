712638d0c0dd8b9b25421290d1c39b13098d3805
William Wolff <wolff.william@pm.me>
2024-07-22 15:55:23 +0200

    feat: draft of projection demo

---
+++ credential-indexer/README.md
@@ -0,0 +1,3 @@
+# Client credential-based indexing of transactions
+
+This repository makes use of an updated version of the cardano-js-sdk to project credentials for each transaction. The goal is to test querying transactions for a given set of credentials (payment/ stake key hashes).
---
+++ credential-indexer/local-link.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Define the base paths
+CJSSDK_PACKAGE_PATH="$HOME/Workspaces/IO/cardano-js-sdk/packages"
+NODE_MODULES_PATH="$(pwd)/node_modules/@cardano-sdk"
+
+# Package names to be linked locally
+PACKAGES=("core" "ogmios" "projection" "projection-typeorm" "util-rxjs")
+
+create_symlink() {
+    local package=$1
+    echo "Creating symlink for $package..."
+    rm -rf "$NODE_MODULES_PATH/$package"
+    ln -sf "$CJSSDK_PACKAGE_PATH/$package" "$NODE_MODULES_PATH/$package"
+}
+
+# Main script execution
+echo "Starting to create symlinks..."
+
+for package in "${PACKAGES[@]}"; do
+    create_symlink "$package"
+done
+
+echo "Symlink creation completed."
